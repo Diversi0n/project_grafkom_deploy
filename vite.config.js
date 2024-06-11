919d8405a5e675a7c17e3616d06375f132a307d6
Albert Nathanael Hidayat <128572415+Diversi0n@users.noreply.github.com>
2024-06-12 00:36:54 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default {
-  base: '/project_grafkom/', // ganti dengan nama repositori Anda
+  base: '/project_grafkom_deploy/', // ganti dengan nama repositori Anda
 };
